c4d010500f11a8fd759c20aa7556baff871a5ca4
sangjung <sangjung245@gmail.com>
2023-09-17 12:37:24 +0900

    [ADD] Alarm 유틸 추가

---
+++ src/utils/utils.js
@@ -0,0 +1,53 @@
+export const getDate = (date) => {
+    const dt = new Date(date);
+    return {
+        year: dt.getFullYear(),
+        month: dt.getMonth() + 1,
+        day: dt.getDate()
+    };
+};
+
+export const getTime = (time) => {
+    const dt = new Date(time);
+    const timeObj = {
+        hour: dt.getHours(),
+        minute: dt.getMinutes(),
+        second: dt.getSeconds()
+    };
+    const format = timeObj.hour < 12 ? 
+    "AM" : 
+    (() => {
+        timeObj.hour = timeObj.hour === 12 ? 12 : timeObj.hour - 12; 
+        return "PM";
+    })();
+    return {
+        hour: timeObj.hour.toString().padStart(2, " "), 
+        minute: timeObj.minute.toString().padStart(2, "0"), 
+        second: timeObj.second.toString().padStart(2, "0"), 
+        format
+    };
+};
+
+export const getWeek = (week) => {
+    return week.map((item) => {
+        switch (item){
+            case 0:
+                return "일";
+            case 1:
+                return "월";
+            case 2:
+                return "화";
+            case 3:
+                return "수";
+            case 4:
+                return "목";
+            case 5:
+                return "금";
+            case 6:
+                return "토";
+            default:
+                return NaN;
+        }
+    });
+};
+
