3c6fa84ea7fbb89069b0ff7a886f9702196472f4
whitecity01 <kh081104@gmail.com>
2023-12-02 05:04:53 +0900

    [Feat] StartPage 라우팅 추가

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,14 @@
 import LoginPage from '../pages/auth/LoginPage'
 import RegisterPage from '../pages/auth/RegisterPage';
+import StartPage from '../pages/auth/Startpage';
   
   const auth = [
     {
       index: true,
+      element: <StartPage />,
+    },
+    {
+      path: 'login',
       element: <LoginPage />,
     },
     {
