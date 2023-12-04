349a9bfca4be366629fd9819902e3999101394c2
whitecity01 <kh081104@gmail.com>
2023-12-05 04:42:38 +0900

    [Feat] 로그인 입력 및 확인 버튼 추가

--- src/components/auth/login/LoginForm.jsx
+++ src/components/auth/login/LoginForm.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import { Form } from "react-router-dom";
+import "../../../styles/auth/LoginForm.scss";
 const LoginForm = () => {
-    return(
-        <>
-        LoginForm
-        </>
-    )
-}
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  return (
+    <Form method="post">
+      <div className="login-form" >
+        <div className="input-wrapper id-input">
+          <input
+            className="id-form"
+            type="text"
+            value={id}
+            onChange={(e) => {
+              setId(e.target.value);
+            }}
+            placeholder="아이디"
+          />
+        </div>
+        <div className="input-wrapper pw-input">
+          <input
+            className="pw-form"
+            type="password"
+            value={pw}
+            onChange={(e) => {
+              setPw(e.target.value);
+            }}
+            placeholder="비밀번호"
+          />
+        </div>
+
+        <button type="submit" className="login-btn">
+          확인
+        </button>
+      </div>
+    </Form>
+  );
+};
 
 export default LoginForm;
