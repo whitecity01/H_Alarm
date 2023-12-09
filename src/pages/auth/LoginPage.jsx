e3b9eb18fe3f93df6dbff9053644d44901277eef
whitecity01 <kh081104@gmail.com>
2023-12-09 13:04:32 +0900

    [Feat] LoginPage 임시 action 추가

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,13 +1,30 @@
+import { redirect } from "react-router-dom";
 import LoginForm from "../../components/auth/login/LoginForm";
 import LoginLayout from "../../components/auth/login/LoginLayout";
+import { requestLogin } from "../../services/auth/auth";
 
 const LoginPage = () => {
-    return(
-        <div>
-            <LoginLayout/>
-            <LoginForm/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <LoginLayout />
+      <LoginForm />
+    </div>
+  );
+};
 
 export default LoginPage;
+
+export const action = async ({ request }) => {
+  const data = await request.formData();
+
+  const authForm = {
+    id: data.get("id"),
+    password: data.get("pw"),
+  };
+
+  const res = await requestLogin(authForm);
+
+  if (res === 200) {
+    return redirect("/home");
+  }
+};
