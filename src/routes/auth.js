edff988c3afe0b47d8dc31da8cf12d2c2e88f050
whitecity01 <kh081104@gmail.com>
2023-12-09 13:04:56 +0900

    [Feat] router loginAction 추가

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,20 +1,21 @@
-import LoginPage from '../pages/auth/LoginPage'
-import RegisterPage from '../pages/auth/RegisterPage';
-import StartPage from '../pages/auth/Startpage';
-  
-  const auth = [
-    {
-      index: true,
-      element: <StartPage />,
-    },
-    {
-      path: 'login',
-      element: <LoginPage />,
-    },
-    {
-      path: 'register',
-      element: <RegisterPage />,
-    },
-  ];
-  
-  export default auth;
+import { LoginPage, login as loginAction } from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
+import StartPage from "../pages/auth/Startpage";
+
+const auth = [
+  {
+    index: true,
+    element: <StartPage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+    action: loginAction,
+  },
+  {
+    path: "register",
+    element: <RegisterPage />,
+  },
+];
+
+export default auth;
