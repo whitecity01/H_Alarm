91428e786c9eff3eb7ea47c0c7eb7647290481fa
whitecity01 <kh081104@gmail.com>
2023-12-10 18:24:49 +0900

    [Feat] RegisterPage action 추가[임시]

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,5 +1,7 @@
+import { redirect } from "react-router-dom";
 import RegisterForm from "../../components/auth/register/RegisterForm";
 import RegisterLayout from "../../components/auth/register/RegisterLayout";
+import { requestRegister } from "../../services/auth/auth";
 
 const RegisterPage = () => {
   return (
@@ -11,3 +13,20 @@ const RegisterPage = () => {
 };
 
 export default RegisterPage;
+
+export const action = async ({ request }) => {
+  const data = await request.formData();
+
+  const authForm = {
+    emil: data.get("email-form"),
+    phoneNumber: data.get("phone-number-form"),
+    password: data.get("pw-form"),
+  };
+
+  const res = await requestRegister(authForm);
+
+  if (res === 200) {
+    window.alert("회원가입이 완료되었습니다.");
+    return redirect("/home");
+  }
+};
