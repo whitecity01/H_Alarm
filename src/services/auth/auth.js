457e880c5e2f7c7fee9f0dbf1df8193339868309
whitecity01 <kh081104@gmail.com>
2023-12-12 22:46:56 +0900

    [Feat] 이메일, 전화번호 인증 api 추가

--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -1,5 +1,5 @@
-import axios from "axios"
-import { SERVER_IP } from "../../constants/api"
+import axios from "axios";
+import { SERVER_IP } from "../../constants/api";
 
 /**
  * auth API 요청 Interface
@@ -9,29 +9,36 @@ import { SERVER_IP } from "../../constants/api"
  * @returns 응답 객체
  */
 const authAPIInterface = async (url, data = null, params = null) => {
-    try {
-        const res = await axios.post(`${SERVER_IP}${url}`, data, {
-            headers: { "Content-Type": "application/json" },
-        })
+  try {
+    const res = await axios.post(`${SERVER_IP}${url}`, data, {
+      headers: { "Content-Type": "application/json" },
+    });
 
-        console.log(res);
-        return res.data;
-    }
-    catch (e) {
-        console.error("emergency : ", e);
-    }
-
-}
+    console.log(res);
+    return res.data;
+  } catch (e) {
+    console.error("emergency : ", e);
+  }
+};
 
 const requestLogin = async (data) => {
-    const res = await authAPIInterface('/login', data);
-    return res;
-}
+  const res = await authAPIInterface("/login", data);
+  return res;
+};
 
 const requestRegister = async (data) => {
-    const res = await authAPIInterface('/register', data);
-    return res;
-}
+  const res = await authAPIInterface("/register", data);
+  return res;
+};
 
+const emailVerify = async (data) => {
+  const res = await authAPIInterface("/email-verify", data);
+  return res;
+};
 
-export { requestLogin, requestRegister };
+const phoneNumberVerify = async (data) => {
+  const res = await authAPIInterface("/phone-number-verify", data);
+  return res;
+};
+
+export { requestLogin, requestRegister, emailVerify, phoneNumberVerify };
