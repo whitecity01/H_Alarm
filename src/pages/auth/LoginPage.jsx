a319fd297804112412a904f462aef64f79b6ebaa
whitecity01 <kh081104@gmail.com>
2023-12-22 23:52:13 +0900

    [Feat] 로그인 성공 시 토큰 처리 로직 추가

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -2,6 +2,7 @@ import { redirect } from "react-router-dom";
 import LoginForm from "../../components/auth/login/LoginForm";
 import LoginLayout from "../../components/auth/login/LoginLayout";
 import { requestLogin } from "../../services/auth/auth";
+import { getAuthToken, setRefreshToken } from "../../utils/token";
 
 const LoginPage = () => {
   return (
@@ -25,6 +26,15 @@ export const action = async ({ request }) => {
   const res = await requestLogin(authForm);
 
   if (res === 200) {
+    const { setAccessToken } = getAuthToken();
+    const { accessTokenDto, refreshTokenDto } = res.data;
+
+    const { accessToken } = accessTokenDto;
+    const { refreshToken, refreshTokenExpiredTime: expiredTime } =
+      refreshTokenDto;
+    setAccessToken(accessToken);
+    setRefreshToken(refreshToken, expiredTime);
+
     return redirect("/home");
   }
 };
