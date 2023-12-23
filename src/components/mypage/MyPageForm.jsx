a352128ad2342fc951a6c16c8ce48ff484e97ddd
whitecity01 <kh081104@gmail.com>
2023-12-24 03:06:23 +0900

    [Feat] EditUserInfo, EditAlarmMethod 컴포넌트 추가

---
+++ src/components/mypage/MyPageForm.jsx
@@ -0,0 +1,13 @@
+import EditAlarmMethod from "./EditAlramMethod";
+import EditUserInfo from "./EditUserInfo";
+import "../../styles/mypage/MyPageForm.scss";
+const MyPageForm = () => {
+  return (
+    <div className="mypage-form-container">
+      <EditUserInfo />
+      <EditAlarmMethod />
+    </div>
+  );
+};
+
+export default MyPageForm;
