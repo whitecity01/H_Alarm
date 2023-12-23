47938e76b7d040df21303ecb51d4f8ca103b16eb
whitecity01 <kh081104@gmail.com>
2023-12-24 03:06:46 +0900

    [Feat] MyPageLayout 컴포넌트 추가

---
+++ src/components/mypage/MyPageLayout.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "../../styles/mypage/MyPageLayout.scss";
+
+const MyPageLayout = () => {
+  return (
+    <div className="mypage-layout-container">
+      <Link className="mypage-back-link" to="/alarm">
+        &lt;
+      </Link>
+      <span className="mypage-title">마이페이지</span>
+      <Link className="logout-link" to="/">
+        <img className="logout-btn" src="/logout_btn.png" alt="Logout" />
+      </Link>
+    </div>
+  );
+};
+
+export default MyPageLayout;
