779bec47d58927c644b84bf65b8b90ad67d7942d
sangjung <sangjung245@gmail.com>
2023-12-24 14:17:37 +0900

    [REFACTOR] Refactor Header and Header's Adapters component code

--- src/components/alarm/AlarmHeader.js
+++ src/components/alarm/AlarmHeader.js
@@ -1,18 +1,15 @@
-import { useCallback } from 'react';
-
 import Header from './Header';
 
-const AlarmHeader = ({isEdit, setIsEdit, setIsAddAlarm}) => {
-    const leftClick = useCallback(()=>{
-        setIsEdit(edit => !edit);
-    },[setIsEdit])
-    const rightClick = useCallback(()=>{
-        setIsAddAlarm(isAddAlarm => !isAddAlarm);
-    },[setIsAddAlarm])
-    const left = isEdit ? "Done" : "Edit";
-    
+/**
+ * AlarmHeader 컴포넌트. Header 컴포넌트를 활용하기 위한 어뎁터 컴포넌트. 일반적인 상황에서의 헤더
+ * @param {Object} param - AlarmHeader 컴포넌트의 props
+ * @param {Function} param.setIsAddAlarm - +를 클릭했을때 수행할 이벤트 함수
+ * @returns {JSX.Element} Header 컴포넌트 렌더링
+ */
+const AlarmHeader = ({setIsAddAlarm}) => {
+    const leftClick = ()=> setIsAddAlarm(true);
     return(
-        <Header left={left} center={"Alarm"} right={"+"} leftClick={leftClick} rightClick={rightClick}/>
+        <Header left={"+"} right={null} leftClick={leftClick} rightClick={null}/>
     )
 }
 
--- src/components/alarm/Header.js
+++ src/components/alarm/Header.js
@@ -2,25 +2,36 @@ import classNames from 'classnames/bind';
 import headerStyle from './header.module.scss';
 const style = classNames.bind(headerStyle);
 
-const Header = ({left, center,right, leftClick, centerClick, rightClick}) => {
+/**
+ * Header 컴포넌트. 페이지 상단에 위치, 왼쪽 및 오른쪽의 문자열과, 클릭 이벤트 표시
+ * 
+ * @param {Object} param - Header 컴포넌트의 props
+ * @param {string} param.left - 왼쪽 영역에 표시될 텍스트
+ * @param {string} param.right - 오른쪽 영역에 표시될 텍스트. 값이 없으면 대신 원형 요소가 표시
+ * @param {Function} param.leftClick - 왼쪽 영역이 클릭될 때 호출되는 함수
+ * @param {Function} param.rightClick - 오른쪽 영역이 클릭될 때 호출되는 함수
+ * @returns {JSX.Element} Header 컴포넌트를 렌더링
+ */
+const Header = ({left, right, leftClick, rightClick}) => {
+
+    const RightBox = () => right ? <span>{right}</span> : <div className={style('circle')}/>;
 
     return(
-        <>
-        <div className={style('margin')}/>
-        <div className={style('container')}>
-            <div className={style('header')}>
-                <div className={style('edit')}>
-                    <span onClick={leftClick}>{left}</span>
-                </div>
-                <div className={style('title')}>
-                    <span onClick={centerClick}>{center}</span>
-                </div>
-                <div className={style('plus')}>
-                    <span onClick={rightClick}>{right}</span>
-                </div>
+        <div className={style('header')}>
+            <div className={style('left')} onClick={leftClick}>
+                <span>{left}</span>
             </div>
-        </div>    
-        </>
+            <div className={style('center')}>
+                <p className={style('logo')}>
+                    <span>G</span>
+                    <span>F</span>
+                    <span>UP</span>
+                </p>
+            </div>
+            <div className={style('right')} /*onclick={rightClick}*/>
+                <RightBox/>
+            </div>
+        </div>
     )
 }
 
