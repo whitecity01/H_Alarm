4b0edac971ec2b15447191c4de79248d38a20369
SangJung <sangjung245@gmail.com>
2024-01-22 14:54:35 +0900

    [RENAAME] valid.js -> validate.js 로 수정

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,14 +1,14 @@
-export const isEmail = email => {
+export const validateEmail = email => {
     const re = /\S+@\S+\.\S+/;
     return re.test(email) && email.length <= 254;
 }
 
-export const isPhoneNumber = phoneNumber => {
+export const validatePhoneNumber = phoneNumber => {
     const re = /^\d{3}-\d{3,4}-\d{4}$/;
     return re.test(phoneNumber);
 }
 
-export const isPassword = password => {
+export const validatePassword = password => {
     return true;
     // const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,30}$/;
     // return re.test(password) && password.length >= 8 && password.length <= 30;
