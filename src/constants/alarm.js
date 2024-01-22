fc1d4fd11fe0852c38b91c5f01d6db50d39b44b5
SangJung <sangjung245@gmail.com>
2024-01-22 14:56:16 +0900

    [FEAT] 알람 페이지에서 사용하는 상수 추가

--- src/constants/alarm.js
+++ src/constants/alarm.js
@@ -1,11 +1,22 @@
-export const ALL_LOAD = 0;
-export const LOAD_MULTIPLE = 1;
-export const LOAD_SINGLE = 2;
-export const CREATE_ALARM = 3;
-export const REMOVE_ALARM = 4;
-export const UPDATE_ALARM = 5;
+export const ALL_LOAD = -1;
+export const LOAD_MULTIPLE = -2;
+export const LOAD_SINGLE = -3;
+export const CREATE_ALARM = -4;
+export const REMOVE_ALARM = -5;
+export const UPDATE_ALARM = -6;
 
 export const LOAD_VALUE = 30;
 
-export const EMPTY_ALARM_LIST = -1;
+export const EMPTY_ALARM_LIST = 0;
 
+export const ALARM_DTO_EMAIL = "EMAIL";
+export const ALARM_DTO_CALL = "CALL";
+export const ALARM_DTO_SMS = "SMS";
+
+export const ALARM_DTO_SUN = "SUN";
+export const ALARM_DTO_MON = "MON";
+export const ALARM_DTO_TUE = "TUE";
+export const ALARM_DTO_WED = "WED";
+export const ALARM_DTO_THU = "THU";
+export const ALARM_DTO_FRI = "FRI";
+export const ALARM_DTO_SAT = "SAT";
